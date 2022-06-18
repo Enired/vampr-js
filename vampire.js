f882b63e42964e9c3e2172442d01ecefd1e1c7cb
Derine Banis <d.banis.13@gmail.com>
2022-06-17 20:08:14 -0700

    Assignment Complete

--- vampire.js
+++ vampire.js
@@ -10,21 +10,39 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
+    return this.offspring.length;
 
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let count = 0;
+    let currentVamp = this;
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      count++;
+    }
+    return count;
 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    const thisVampireDistance = this.numberOfVampiresFromOriginal;
+    const otherVampireDistance = vampire.numberOfVampiresFromOriginal;
+
+    if (thisVampireDistance < otherVampireDistance) {
+      return true;
+    } else {
+      return false;
+    }
+
 
   }
 
@@ -36,6 +54,57 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let youngestVampire;
+    let olderVampire;
+
+    if (!this.creator || !vampire.creator) { //Comparing anything with root.
+      if (!this.creator) {
+        return this;
+      } else {
+        return vampire;
+      }
+    }
+
+    if (this.name === vampire.name) { //Comparing self
+      return this;
+    }
+
+    if (this.isMoreSeniorThan(vampire)) { //Determining who is the younger vampire between the two
+      youngestVampire = vampire;
+      olderVampire = this;
+    } else {
+      youngestVampire = this;
+      olderVampire = vampire;
+    }
+
+    while (youngestVampire.creator) {
+      if (youngestVampire.creator.name === olderVampire.name) { //Descendant
+        return olderVampire;
+      } else if (youngestVampire.creator.name === olderVampire.creator.name) { //Share the same creator
+        return olderVampire.creator;
+      } else {
+        youngestVampire = youngestVampire.creator;
+      }
+    }
+
+    return youngestVampire; //Root Case
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   }
 }
